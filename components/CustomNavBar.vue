e930e8f8591c0e9099445e3e32496de803f17eed
zdesign <1959018962@qq.com>
2024-07-03 22:15:29 +0800

    update

---
+++ components/CustomNavBar.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="custom_navbar">
+		<view style="height:36px;"></view>
+		<uni-nav-bar :left-text="leftText" :right-text="rightText" :title="title" :color="color" :background-color="backgroundColor" @clickLeft="clickLeft" @clickRight="clickRight" />
+	</view>
+</template>
+
+<script>
+export default {
+	name:"CustomNavBar",
+	props: {
+		leftText: {
+			type: String,
+			default: ''
+		},
+		rightText: {
+			type: String,
+			default: ''
+		},
+		title: {
+			type: String,
+			default: ''
+		},
+		color: {
+			type: String,
+			default: '#f1f1f1'
+		},
+		backgroundColor: {
+			type: String,
+			default: '#0088ff'
+		}
+	},
+	data() {
+		return {
+			
+		};
+	},
+	methods: {
+		clickLeft() {
+			alert(1)
+		},
+		clickRight() {
+			alert(2)
+		}
+	},
+}
+</script>
+
+<style scoped>
+.custom_navbar {
+	
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="container">
-		<uni-nav-bar left-text="返回" right-text="设置" title="自定义标题" color="#f1f1f1" background-color="#0088ff" />
+		<CustomNavBar left-text="返回" rightText="设置" title="门户首页" />
 		<uni-notice-bar style="margin-bottom: 0px;" showClose showIcon scrollable single text="[多行] 这是 NoticeBar 通告栏，这是 NoticeBar 通告栏，这是 NoticeBar 通告栏，这是 NoticeBar 通告栏"></uni-notice-bar>
 		<uni-swiper-dot :info="info" :current="current" field="content" mode="default">
 			<swiper style="padding: 5px;" @change="change">
@@ -15,11 +15,18 @@
 		<uni-section title="个人信息" type="line">
 			个人信息
 		</uni-section>
+		<!-- 升级中心 -->
+		<UpgradeCenter />
 	</view>
 </template>
 
 <script>
+import UpgradeCenter from '@/components/UpgradeCenter.vue'
+import CustomNavBar from '@/components/CustomNavBar.vue'
 export default {
+	components: {
+		UpgradeCenter,CustomNavBar
+	},
 	data() {
 		return {
 			info: [
@@ -28,7 +35,13 @@ export default {
 				'https://cdn.uviewui.com/uview/swiper/swiper3.png'
 			],
 			current: 0,
+			statusBarHeight: 0
 		}
+	},
+	onLoad() {
+		const systemInfo = uni.getSystemInfoSync();
+		console.log(systemInfo);
+		this.statusBarHeight = systemInfo.platform === 'android'? 30 : (systemInfo.platform === 'ios'?20:0)
 	},
 	methods: {
 		change(e) {
